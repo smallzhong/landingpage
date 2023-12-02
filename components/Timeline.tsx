e6db3c506f48ed502a053c0d06626a3ecd2c07ae
LightQuantum <self@lightquantum.me>
2023-12-02 05:47:59 -0800

    Set cache policy

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -6,8 +6,8 @@ import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 export const Timeline = async ({ id }: { id: string }) => {
   const host = headers().get("host");
   const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
-  try{
-    const tweetIds: string[] = await (await fetch(`${protocol}://${host}/api/tweet-ids?user=${id}`, { cache: "no-cache" })).json();
+  try {
+    const tweetIds: string[] = await (await fetch(`${protocol}://${host}/api/tweet-ids?user=${id}`, { next: { revalidate: 1800 } })).json();
     return (
       <div className="flex flex-col space-y-6">
         {tweetIds.map((tweetId, i) => (
@@ -18,10 +18,9 @@ export const Timeline = async ({ id }: { id: string }) => {
         ))}
       </div>
     );
-  }
-  catch (e) {
+  } catch (e) {
     return (
       <p className="text-ctp-red">! Failed to load timeline</p>
-    )
+    );
   }
 };
